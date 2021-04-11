3813b06c40f96d6251c3f1dfdb5316b065f6b736
minjoo <minjoo9812@gmail.com>
2021-04-11 12:15:24 +0900

    10주차

--- 10주차/텀 프로젝트/김민주/텀프로젝트_BJ9466.java
+++ 10주차/텀 프로젝트/김민주/텀프로젝트_BJ9466.java
@@ -8,52 +8,47 @@ import java.util.StringTokenizer;
 public class 텀프로젝트_BJ9466 {
 
 	static int n, cnt;
-	static int [] student;
-	static boolean [] check;
-	static boolean [] visited;
+	static int[] student;
+	static boolean[] check;
+	static boolean[] visited;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(in.readLine());
-		for(int tc = 1; tc<=T; tc++) {
+		for (int tc = 1; tc <= T; tc++) {
 			n = Integer.parseInt(in.readLine());
-			student = new int [n+1];
-			check = new boolean [n+1];
-			visited = new boolean [n+1];
+			student = new int[n + 1];
+			check = new boolean[n + 1];
+			visited = new boolean[n + 1];
 			cnt = 0;
 			StringTokenizer st = new StringTokenizer(in.readLine());
-			for(int i = 1; i<=n; i++) {
+			for (int i = 1; i <= n; i++) {
 				student[i] = Integer.parseInt(st.nextToken());
-//				if(student[i] == i) {
-//					check[i] = true; // 사이클이 본인 혼자 돌면 우선 체크
-//					cnt++;
-//				}
+				if (student[i] == i) {
+					check[i] = true; // 사이클이 본인 혼자 돌면 우선 체크
+					cnt++;
+				}
 			}
-			for(int i = 1; i<=n; i++) {
+			for (int i = 1; i <= n; i++) {
 				dfs(i);
 			}
-			
-			System.out.println(n-cnt);
+
+			System.out.println(n - cnt);
 		}
 	}
-	
+
 	public static void dfs(int x) {
-		if(check[student[x]] || check[x]) return; //이미 사이클 형성 되어있으면 바로 return
-		
+		if (check[student[x]] || check[x])
+			return; // 이미 사이클 형성 되어있으면 바로 return
+
 		visited[x] = true;
-		if(!visited[student[x]]) 
-			dfs(student[x]); //현재 학생이 가리키는 학생 탐색
-		else{
-//			int cycle = x; // 시간초과
-//			cnt++;
-//			while(!check[x]) {
-//				x = student[x];
-//				cnt++;
-//			}
-			
-			if(!check[student[x]]) {
+		if (!visited[student[x]])
+			dfs(student[x]); // 현재 학생이 가리키는 학생 탐색
+		else {
+			if (!check[student[x]]) {
 				cnt++;
 //				사이클을 이루는 간선의 수 cnt++
-				for(int i = student[x]; i != x; i = student[i]) {
+				for (int i = student[x]; i != x; i = student[i]) {
 //					System.out.println("student[x] = "+i);
 //					student[6] = 4. => i=4
 //					그 다음 i는 student[i] = student[4] = 7
@@ -63,8 +58,7 @@ public class 텀프로젝트_BJ9466 {
 				}
 			}
 		}
-		
+
 		check[x] = true;
 	}
 }
-
